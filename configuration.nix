aa4e32363deabddef0d0e849c9522e9375ab9476
joe-broder15 <sharp@JBox.>
2026-02-17 18:53:34 -0800

    added tavern

--- configuration.nix
+++ configuration.nix
@@ -7,7 +7,7 @@
 {
   imports = [
     # Include the results of the hardware scan.
-    /etc/nixos/hardware-configuration.nix
+    ./hardware-configuration.nix
     ./proxy.nix
     ./homer.nix
   ];
