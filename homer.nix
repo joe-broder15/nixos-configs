e3917c95f0a1ae1f87fc84d9ebd6df396de6d36f
joe-broder15 <sharp@JBox.>
2026-02-21 11:02:31 -0800

    added icons to homer

--- homer.nix
+++ homer.nix
@@ -17,15 +17,18 @@
           items = [
             {
               name = "Plex";
+              icon = "fab fa play";
               url = "https://plex.local.clubtropicalexcellent.vip";
             }
             {
               name = "qBittorrent";
+              icon = "fab fa skull-crossbones";
               url = "https://qBittorrent.local.clubtropicalexcellent.vip";
 
             }
             {
               name = "SillyTavern";
+              icon = "fab fa beer-mug-empty";
               url = "https://sillytavern.local.clubtropicalexcellent.vip";
             }
           ];
